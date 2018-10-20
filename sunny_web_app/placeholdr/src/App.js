a20767f7edfdf5f14a4970c2fd04305288234f11
sunkakar <sundeepkakar@gmail.com>
2018-10-20 01:41:36 -0400

    npm installed react

---
+++ sunny_web_app/placeholdr/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="App-header">
+        <img alt="lol.jpeg" display="inline" height="100px" width="100px" src = {logo}/>
+        <h1>PlaceHoldr</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
