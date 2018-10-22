ad478a66689d4ec43cf76576f8bfa4e7ecee8fb6
hansenmax <hansenmt@purdue.edu>
2018-10-22 17:17:53 -0400

    Adding aditional activity for sign in

---
+++ max_mobile_app/PlaceHoldr/app/src/main/java/com/example/max/placeholdr/signIn.java
@@ -0,0 +1,13 @@
+package com.example.max.placeholdr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
